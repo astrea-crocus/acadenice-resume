3706dc6550f232a835c8937922c7fbf77dcd335a
favale-cloe <cloe.favale@gmail.com>
2025-07-29 16:55:17 +0200

    ui(builder) petite modification

--- apps/client/src/pages/builder/sidebar/sections/publish/index.tsx
+++ apps/client/src/pages/builder/sidebar/sections/publish/index.tsx
@@ -8,15 +8,15 @@ export const Publish = () => {
     <div className="flex">
       <div className="h-screen flex-1 pb-16 lg:px-4 lg:py-2">
         <div className="flex h-full flex-col @container/right">
-          <div className="h-1/3 bg-red-500/50" />
+          <div className="h-1/3" />
 
-          <div className="flex flex-col bg-blue-500/50 p-6">
+          <div className="flex flex-col p-6">
             <ExportSection />
             <Separator />
             <SharingSection />
           </div>
 
-          <div className="h-1/3 bg-red-500/50" />
+          <div className="h-1/3" />
         </div>
       </div>
     </div>
